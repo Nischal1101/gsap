7ec504b34317e22af20463eaea59f667a41007c5
nischal1101 <kaflenk123@gmail.com>
2024-10-10 07:21:46 +0545

    learn scroll trigger

--- src/components/ScrollTriggerPage.tsx
+++ src/components/ScrollTriggerPage.tsx
@@ -1,7 +1,9 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const ScrollTriggerPage = () => {
+  gsap.registerPlugin(ScrollTrigger);
   useGSAP(() => {
     gsap.from("#page1 #box", {
       scale: 0,
@@ -12,8 +14,17 @@ const ScrollTriggerPage = () => {
     gsap.from("#page2 #box", {
       scale: 0,
       duration: 2,
-      delay: 1,
-      rotate: 360,
+      rotate: 720,
+
+      scrollTrigger: {
+        trigger: "#page2 #box",
+        scroller: "body",
+        markers: true,
+        start: "top 60%",
+        end: "bottom 30%",
+        scrub: 2,
+        pin: true,
+      },
     });
   }, []);
   return (
@@ -26,7 +37,7 @@ const ScrollTriggerPage = () => {
       </div>
       <div
         id="page2"
-        className="h-screen w-full bg-blue-400 flex items-center justify-center"
+        className="h-screen w-full bg-blue-400 flex flex-col gap-5 items-center justify-center"
       >
         <div id="box" className="h-[300px] w-[300px] bg-red-400"></div>
       </div>
